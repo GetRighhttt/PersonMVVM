0fd825370858d4e743358bb3312225d657e0a8ba
Stefan Bayne <stefanbayne@gmail.com>
2022-11-17 08:38:09 -0500

    First commit.

---
+++ UsersJsonPlaceholder/UsersJsonPlaceholder/UsersJsonPlaceholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersJsonPlaceholderApp.swift
+//  UsersJsonPlaceholder
+//
+//  Created by Stefan Bayne on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersJsonPlaceholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
